a82250865db7605c2a80b4b96c80e03a64573e2f
MiloAC1 <crabapplefarms@icloud.com>
2024-10-27 19:36:11 -0500

    Modeled Standard Layout; Started nesting

--- src/routes/research/+pages.svelte
+++ src/routes/research/+pages.svelte
@@ -10,17 +10,15 @@
 
     <div class="flex-col min-w-[850px] min-h-screen text-white pt-24 ">
         <h1 class="text-3xl font-publicsans mb-8" style="font-variation-settings: 'wght' 700;">
-            Welcome
+            Research
         </h1>
-        <p class="mt-4">
-            Hello, I am Milo, a self taught developer passionate about design.
-        </p>
-        <p class="mt-4">
-            Currenlty I am pursuing projects and reasearch in different languages.
-        </p>
+        <div>
+        </div>
     </div>
 
     <div class="flex-col w-80 min-h-screen text-white p-8 pt-24">
         <p>test</p>
+        <div>
+        </div>
     </div>
 </div>
