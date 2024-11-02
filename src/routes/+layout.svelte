2ec1260251f223cc8bfec427944ca9ecb8822255
MiloAC1 <crabapplefarms@icloud.com>
2024-11-02 15:28:18 -0500

    Made main use status navigation and per page rendering

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,21 +1,6 @@
 <script lang="ts">
-    import Sidebar from '$lib/Sidebar.svelte'
 	import '../app.css';
 	let { children } = $props();
 </script>
 
-<div class="flex flex-row gap-1 justify-center font-karla text-[16px]">
-    <div class="flex-col w-80 min-h-screen text-white p-8 pt-24">
-        <Sidebar/>
-
-    </div>
-
-    <div class="flex-col min-w-[850px] min-h-screen text-white pt-24 ">
-        {@render children()}
-   </div>
-
-    <div class="flex-col w-80 min-h-screen text-white p-8 pt-24">
-        <p>test</p>
-    </div>
-</div>
-
+{@render children()}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,24 @@
-<div class="flex flex-col justify-center">
+<div class="flex flex-row gap-1 justify-center font-karla text-base text-white">
+	<div class="flex-col w-80 min-h-screen p-8 pt-24">
+        <!-- Sidebar -->
+			<a href="/">
+				<h1 class="text-3xl font-publicsans mb-8" style="font-variation-settings: 'wght' 700;">
+					MiloAC
+				</h1>
+			</a>
+	</div>
 
-        <h1 class="text-3xl font-publicsans mb-8" style="font-variation-settings: 'wght' 700;">
-            Welcome
-        </h1>
-        <p class="mt-4">
-            Hello, I am Milo, a self taught developer passionate about design.
-        </p>
-        <p class="mt-4">
-            Currenlty I am pursuing projects and reasearch in different languages.
-        </p>
-    </div>
+	<div class="flex-col min-w-[850px] min-h-screen pt-36">
+        <!-- Main -->
+			<h1 class="text-3xl font-publicsans mb-8" style="font-variation-settings: 'wght' 700;">
+				Welcome
+			</h1>
+			<p class="mt-4">Hello, I am Milo, a self taught developer passionate about design.</p>
+			<p class="mt-4">Currenlty I am pursuing projects and reasearch in different languages.</p>
+	</div>
+
+	<div class="flex-col w-80 min-h-screen p-8 pt-24">
+        <!-- Sidebar -->
+		<p>test</p>
+	</div>
+</div>
