5111f8a3b4ca8b8ce95f771a078e8f313f3fcaa2
MiloAC1 <crabapplefarms@icloud.com>
2024-11-02 20:42:52 -0500

    Updated tailwind to be more concise; Fixed layout to be more concise

--- src/routes/Research/+page.svelte
+++ src/routes/Research/+page.svelte
@@ -1,33 +1,23 @@
-<script>
-import tilt from '$lib/tilt.js'
-</script>
-<div class="flex flex-row gap-1 justify-center font-karla text-[16px]">
-    <div class="flex-col w-80 min-h-screen text-white p-8 pt-24">
-        <h1 class="text-3xl font-publicsans" style="font-variation-settings: 'wght' 700;">
-            MiloAC
-        </h1>
-        <div>
+<div class="flex flex-row gap-1 justify-center font-karla text-base text-white">
+	<div class="flex-col w-80 min-h-screen p-8 pt-24">
+        <!-- Sidebar -->
+			<a href="/">
+				<h1 class="text-3xl font-publicsans mb-8" style="font-variation-settings: 'wght' 700;">
+					MiloAC
+				</h1>
+			</a>
+	</div>
 
-        </div>
-    </div>
+	<div class="flex-col min-w-[850px] min-h-screen pt-36">
+        <!-- Main -->
+			<h1 class="text-3xl font-publicsans mb-8" style="font-variation-settings: 'wght' 700;">
+				Research
+			</h1>
+	</div>
 
-    <div class="flex-col min-w-[850px] min-h-screen text-white pt-24 ">
-        <h1 class="text-3xl font-publicsans mb-8" style="font-variation-settings: 'wght' 700;">
-            Research
-        </h1>
-        <div>
-<div use:tilt={{ scale: 1}} class="h-[600px] w-[300px] bg-blue-50">
-	<h3>Svelte Tilt!</h3>
-	<p>
-		Just hover!
-	</p>
+	<div class="flex-col w-80 min-h-screen p-8 pt-24">
+        <!-- Sidebar -->
+		<p>test</p>
+	</div>
 </div>
-        </div>
-    </div>
 
-    <div class="flex-col w-80 min-h-screen text-white p-8 pt-24">
-        <p>test</p>
-        <div>
-        </div>
-    </div>
-</div>
