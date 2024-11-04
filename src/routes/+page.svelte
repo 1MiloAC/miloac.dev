17ada4ae692c78573dff7ed0c7d3ededd3f2c6c0
MiloAC1 <crabapplefarms@icloud.com>
2024-11-03 18:38:09 -0600

    Updated Main page sidebar

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,24 +1,41 @@
 <div class="flex flex-row gap-1 justify-center font-karla text-base text-white">
 	<div class="flex-col w-80 min-h-screen p-8 pt-24">
-        <!-- Sidebar -->
-			<a href="/">
-				<h1 class="text-3xl font-publicsans mb-8" style="font-variation-settings: 'wght' 700;">
-					MiloAC
-				</h1>
-			</a>
+		<!-- Sidebar -->
+		<a href="/">
+			<h1 class="text-3xl font-publicsans mb-8" style="font-variation-settings: 'wght' 700;">
+				MiloAC
+			</h1>
+		</a>
+		<div class="flex">
+			<p
+				class="pt-4 text-transparent text-emerald-500"
+				style="font-variation-settings: 'wght' 700;"
+			>
+				Navigation
+			</p>
+		</div>
+        <a href="./Research">
+		<div class="flex pl-1">
+			<p
+				class="pt-3 text-transparent bg-clip-text bg-gradient-to-r from-emerald-400 via-slate-500 to-slate-500"
+			>
+				Recent Readings
+			</p>
+		</div>
+            </a>
 	</div>
 
 	<div class="flex-col min-w-[850px] min-h-screen pt-36">
-        <!-- Main -->
-			<h1 class="text-3xl font-publicsans mb-8" style="font-variation-settings: 'wght' 700;">
-				Welcome
-			</h1>
-			<p class="mt-4">Hello, I am Milo, a self taught developer passionate about design.</p>
-			<p class="mt-4">Currenlty I am pursuing projects and reasearch in different languages.</p>
+		<!-- Main -->
+		<h1 class="text-3xl font-publicsans mb-8" style="font-variation-settings: 'wght' 700;">
+			Welcome
+		</h1>
+		<p class="mt-4">Hello, I am Milo, a self taught developer passionate about design.</p>
+		<p class="mt-4">Currenlty I am pursuing projects and reasearch in different languages.</p>
 	</div>
 
 	<div class="flex-col w-80 min-h-screen p-8 pt-24">
-        <!-- Sidebar -->
+		<!-- Sidebar -->
 		<p>test</p>
 	</div>
 </div>
