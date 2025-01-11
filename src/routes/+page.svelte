ff6da63c81199d2408c1f381cf9c949eb069dd7d
MiloAC1 <crabapplefarms@icloud.com>
2025-01-11 17:13:35 -0600

    Merging tested change of tw4 upstream code

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,40 +1,116 @@
 <div class="flex flex-row gap-1 justify-center font-karla text-base text-white mx-auto">
-    <!-- Sidebar -->
-	<div class="flex-col min-w-80 min-h-screen p-8 pt-24 lg:block hidden">
-		<a href="/">
-			<h1 class="text-3xl font-publicsans mb-8" style="font-variation-settings: 'wght' 700;">
-				MiloAC
-			</h1>
-		</a>
+	<!-- Sidebar -->
+	<div class="flex-col min-w-80 min-h-screen p-8 md:block hidden">
+
+		<div class="w-32 h-32 py-4"> 
+			<img src="/images/pfp.avif" alt="Link looking pensive" class="rounded-full" />
+		</div>
+
+        <!-- title with blur on hover -->
+		<div class="mb-8 relative group inline-block">
+			<a href="/">
+				<div
+					class=" rounded-full blur-none group-hover:blur-xl group-hover:shadow-xl bg-blend-difference transition-all hover:scale-110 ease-in-out duration-300 group-hover:opacity-100 opacity-0 inline-block"
+				>
+					<h1
+						class="text-3xl font-publicsans inline-block"
+						style="font-variation-settings: 'wght' 700;"
+					>
+						MiloAC
+					</h1>
+				</div>
+				<div class=" absolute top-0">
+					<h1
+						class="text-3xl font-publicsans inline-block z-10 group-hover:scale-110 transition-all duration-300 ease-in-out"
+						style="font-variation-settings: 'wght' 700;"
+					>
+						MiloAC
+					</h1>
+				</div>
+			</a>
+		</div>
+
+    
 		<div class="flex">
 			<p
 				class="pt-4 text-transparent text-emerald-500"
 				style="font-variation-settings: 'wght' 700;"
 			>
 				Navigation
+
 			</p>
 		</div>
-        <a href="./Research">
-		<div class="flex pl-1">
-			<p
-				class="pt-3 text-transparent bg-clip-text bg-gradient-to-r from-emerald-400 via-slate-500 to-slate-500"
-			>
-				Recent Readings
-			</p>
-		</div>
-            </a>
+
+
+
+
+        <div class="ml-1 relative group inline-block">
+		<a href="./TheShelf">
+			<div class="transition-all duration-300 hover:scale-110 ease-in-out">
+				<p
+					class="font-publicsans"
+                    style="font-variation-settings: 'wght' 700;"
+				>
+					The Shelf
+				</p>
+			</div>
+
+				</a>
+</div>
+
+
 	</div>
 
-    <!-- Main -->
-	<div class="flex-col w-[850px] min-w-[450px] max-w-4xl min-h-screen pt-36 px-4 flex-grow break-words overflow-hidden">
-		<h1 class="text-3xl font-publicsans mb-8" style="font-variation-settings: 'wght' 700;">
+	<!-- Main -->
+
+	<div
+		class="flex-col w-[850px] min-w-80 max-w-4xl min-h-screen md:pt-18 pt-4 px-4 grow break-words overflow-hidden"
+	>
+
+        <div class="inline-block">
+            	<div class="visible md:invisible relative group inline-block">
+			<a href="/">
+				<div
+					class=" rounded-full blur-none group-hover:blur-xl group-hover:shadow-xl bg-blend-difference transition-all hover:scale-110 ease-in-out duration-300 group-hover:opacity-100 opacity-0 inline-block"
+				>
+					<h1
+						class="text-3xl font-publicsans inline-block"
+						style="font-variation-settings: 'wght' 700;"
+					>
+						MiloAC
+					</h1>
+				</div>
+				<div class=" absolute top-0">
+					<h1
+						class="text-3xl font-publicsans inline-block z-10 group-hover:scale-110 transition-all duration-300 ease-in-out"
+						style="font-variation-settings: 'wght' 700;"
+					>
+						MiloAC
+					</h1>
+				</div>
+			</a>
+		</div>
+
+
+            </div>
+		<div class="justify-center p-2 mt-5 bg-fixed bg-center">
+			<img src="/images/Frieren.avif" alt="Frieren looking at camera." class="rounded-xl" />
+		</div>
+
+		<h1
+			class="text-xl font-publicsans mt-4 text-emerald-200 inline-block"
+			style="font-variation-settings: 'wght' 700;"
+		>
 			Welcome
 		</h1>
-		<p class="mt-4">Hello, I am Milo, a self taught developer passionate about design.</p>
-		<p class="mt-4">Currenlty I am pursuing projects and reasearch in different languages.</p>
-        	</div>
+		<p class="mt-2">I am Milo, a self taught developer passionate about code and design.</p>
+		<p class="mt-2">
+			Currently I am researching and pursuing several projects in different coding langs, as I
+			pursue a degree in Computer Science.
+		</p>
+	</div>
 
-    <!-- Sidebar -->
+	<!-- Sidebar -->
 	<div class="flex-col w-80 min-h-screen p-8 pt-24 xl:block hidden">
 		<p>test</p>
 	</div>
---
+++ src/routes/TheShelf/+page.svelte
@@ -0,0 +1,86 @@
+<div class="flex flex-row gap-1 justify-center font-karla text-base text-white mx-auto">
+	<!-- Sidebar -->
+	<div class="flex-col min-w-80 min-h-screen p-8 md:block hidden">
+		<div class="w-32 h-32 py-4">
+			<img src="/images/pfp.avif" alt="Link looking pensive" class="rounded-full" />
+		</div>
+
+		<!-- title with blur on hover -->
+		<div class="mb-8 relative group inline-block">
+			<a href="/">
+				<div
+					class=" rounded-full blur-none group-hover:blur-xl group-hover:shadow-xl bg-blend-difference transition-all hover:scale-110 ease-in-out duration-300 group-hover:opacity-100 opacity-0 inline-block"
+				>
+					<h1
+						class="text-3xl font-publicsans inline-block"
+						style="font-variation-settings: 'wght' 700;"
+					>
+						MiloAC
+					</h1>
+				</div>
+				<div class=" absolute top-0">
+					<h1
+						class="text-3xl font-publicsans inline-block z-10 group-hover:scale-110 transition-all duration-300 ease-in-out"
+						style="font-variation-settings: 'wght' 700;"
+					>
+						MiloAC
+					</h1>
+				</div>
+			</a>
+		</div>
+
+		<div class="flex">
+			<p
+				class="pt-4 text-transparent text-emerald-500"
+				style="font-variation-settings: 'wght' 700;"
+			>
+				Navigation
+			</p>
+		</div>
+
+	</div>
+
+	<!-- Main -->
+
+	<div
+		class="flex-col w-[850px] min-w-80 max-w-4xl min-h-screen md:pt-18 pt-4 px-4 grow break-words overflow-hidden"
+	>
+		<div class="inline-block">
+			<div class="visible md:invisible relative group inline-block">
+				<a href="/">
+					<div
+						class=" rounded-full blur-none group-hover:blur-xl group-hover:shadow-xl bg-blend-difference transition-all hover:scale-110 ease-in-out duration-300 group-hover:opacity-100 opacity-0 inline-block"
+					>
+						<h1
+							class="text-3xl font-publicsans inline-block"
+							style="font-variation-settings: 'wght' 700;"
+						>
+							MiloAC
+						</h1>
+					</div>
+					<div class=" absolute top-0">
+						<h1
+							class="text-3xl font-publicsans inline-block z-10 group-hover:scale-110 transition-all duration-300 ease-in-out"
+							style="font-variation-settings: 'wght' 700;"
+						>
+							MiloAC
+						</h1>
+					</div>
+				</a>
+			</div>
+		</div>
+        <div>
+		<h1
+			class="text-xl font-publicsans mt-4 text-emerald-200 inline-block"
+			style="font-variation-settings: 'wght' 700;"
+		>
+			Welcome
+		</h1>
+            </div>
+	</div>
+
+	<!-- Sidebar -->
+	<div class="flex-col w-80 min-h-screen p-8 pt-24 xl:block hidden">
+		<p>test</p>
+	</div>
+</div>
